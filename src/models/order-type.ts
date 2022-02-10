608bdd9812d3687d6bf021f39e15023d7db5e0cb
DimaTch <4dnik@mail.ru>
2022-02-10 13:28:52 +0200

    add ProductOrder type

--- src/models/order-type.ts
+++ src/models/order-type.ts
@@ -1,11 +1,19 @@
+import { ProductOptionConfigured } from "./product-options";
+
 export enum OrderStatus { WAITING="waiting", WORKING="working", COMPLIT="complite" };
 
 export type Order = {
     id?: number;
     userId: string;
     status: OrderStatus;
-    products: {productId:number, options: Object, count:number}[] //TODO product:Product, count:number
+    products: OrderProduct[],
     totalPrice: number;
     dateCreate: string;
+}
+
+export type OrderProduct = {
+    productId:number, 
+    options: ProductOptionConfigured[], 
+    count:number
 }
 
