517169e6a333aa0164e13f984304898939696953
febick <febick29@gmail.com>
2022-02-13 10:13:56 +0200

    tmp

--- src/components/UI/common/navigator.tsx
+++ src/components/UI/common/navigator.tsx
@@ -1,11 +1,18 @@
-import { Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from "@mui/material";
+import Button from '@mui/material/Button';
 import { RouteType } from "../../../models/route-type";
 import NavigatorTabs from "./navigator-tabs";
-import { FC, Fragment, useMemo } from 'react';
+import { FC, Fragment, useEffect, useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import ProfileMenu from "./profile-menu";
 import { PATH_LOGIN } from "../../../config/routing";
 import LoginIcon from '@mui/icons-material/Login';
+import pages from "../../pages";
+import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import { useSelector } from "react-redux";
+import { UserData } from "../../../models/user-data";
+import { userDataSelector } from "../../../redux/store";
 
 type NavigatorProps = {
     logo: string;
@@ -15,23 +22,35 @@ type NavigatorProps = {
 
 const Navigator: FC<NavigatorProps> = (props) => {
 
-    const { logo, menuItems, authItems } = props;
-
+    const userData: UserData = useSelector(userDataSelector);
+    let { logo, menuItems, authItems } = props;
     const path = useLocation().pathname;
-
     const isMenuItem = useMemo(() => menuItems.find(item => item.path === path), [path]);
+    
+    // TODO
+    const categories = [
+        'мясная', 'кошерная', 'морепродукты', 'напитки'
+    ]
+
+    useEffect(() => {
+        if (userData.isAdmin) {
+            // menuItems = categories;
+        }
+    }, [])
+
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
         <Fragment>
-            {isMenuItem && <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-                <Toolbar
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        width: { xs: '100vw', md: '95vw' }
-                    }}
-                >
-                    <Toolbar>
+            { isMenuItem && <AppBar style={{ background: '#ff6f04' }}>
+                <Container maxWidth="xl" >
+                    <Toolbar disableGutters>
                         <Typography
                             variant="h6"
                             noWrap
@@ -40,11 +59,70 @@ const Navigator: FC<NavigatorProps> = (props) => {
                         >
                             {logo}
                         </Typography>
-                        <NavigatorTabs orientation='horizontal' items={menuItems} />
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                {menuItems.map((page) => (
+                                    <MenuItem key={page.path} onClick={handleCloseNavMenu} component={Link} to={page.path} >
+                                        <Typography textAlign="center">{page.label}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                        >
+                            {logo}
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {menuItems.map((page) => (
+                                <Button
+                                    component={Link}
+                                    to={page.path}
+                                    key={page.path}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}>
+                                    {page.label}
+                                </Button>
+                            ))}
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                        { authItems[0].path === PATH_LOGIN ? <Button variant='contained' startIcon={<LoginIcon/ >} component={Link} to={authItems[0].path}>{authItems[0].label}</Button> : <ProfileMenu items={menuItems} userData={userData} /> }
+                        </Box>
                     </Toolbar>
-                    { authItems[0].path === PATH_LOGIN ? <Button startIcon={<LoginIcon/ >} component={Link} to={authItems[0].path}>{authItems[0].label}</Button> : <ProfileMenu items={menuItems} /> }
-                </Toolbar>
-            </Box>}
+                </Container>
+            </AppBar>}
         </Fragment>
     )
 }
